c706704462784e0113f3bd8bf451670e4acbe6f3
GraemeB <grae22@gmail.com>
2019-12-27 10:38:11 +0200

    Added Activity Factory.

---
+++ Activity/Domain/Activity/ActivityFactory.cs
@@ -0,0 +1,41 @@
+﻿// TODO: This class currently provides no way for Activity Types to be given "default tags" on instantiation.
+
+using System;
+using System.Collections.Generic;
+
+using TagLib;
+
+namespace Activity.Domain.Activity
+{
+  internal class ActivityFactory
+  {
+    private readonly ITagFactory _tagFactory;
+    private readonly Dictionary<string, ActivityType> _activitiesByName = new Dictionary<string, ActivityType>(StringComparer.OrdinalIgnoreCase);
+
+    public ActivityFactory(
+      in ITagFactory tagFactory)
+    {
+      _tagFactory = tagFactory ?? throw new ArgumentNullException(nameof(tagFactory));
+    }
+
+    public ActivityType GetOrCreate(
+      in string name)
+    {
+      if (_activitiesByName.ContainsKey(name))
+      {
+        return _activitiesByName[name];
+      }
+
+      IReadOnlyTagBag defaultTags = _tagFactory.CreateTagBag();
+
+      var newActivity = new ActivityType(
+        _tagFactory,
+        name,
+        defaultTags);
+
+      _activitiesByName.Add(name, newActivity);
+
+      return newActivity;
+    }
+  }
+}
---
+++ ActivityTests/Domain/Activity/ActivityFactoryTests.cs
@@ -0,0 +1,46 @@
+﻿using Activity.Domain.Activity;
+
+using NUnit.Framework;
+
+using TagLib;
+
+namespace ActivityTests.Domain.Activity
+{
+  [TestFixture]
+  public class ActivityFactoryTests
+  {
+    [Test]
+    public void Given_NoActivitiesCreated_When_ActivityIsCreated_Then_ActivityInstanceIsReturned()
+    {
+      // Arrange.
+      const string activityName = "activity";
+
+      var tagFactory = new TagFactory();
+      var testObject = new ActivityFactory(tagFactory);
+
+      // Act.
+      ActivityType result = testObject.GetOrCreate(activityName);
+
+      // Assert.
+      Assert.IsNotNull(result);
+    }
+
+    [Test]
+    public void Given_ActivityCreatedPreviously_When_ActivityIsCreatedAgain_Then_SameActivityInstanceIsReturned()
+    {
+      // Arrange.
+      const string activityName = "activity";
+
+      var tagFactory = new TagFactory();
+      var testObject = new ActivityFactory(tagFactory);
+
+      ActivityType activity1 = testObject.GetOrCreate(activityName);
+
+      // Act.
+      ActivityType activity2 = testObject.GetOrCreate(activityName);
+
+      // Assert.
+      Assert.AreSame(activity1, activity2);
+    }
+  }
+}
